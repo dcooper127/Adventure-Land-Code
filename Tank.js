0ee5f4c66248cfded7745535228a5a3bf65e6257
dcooper127 <dcooper127@gmail.com>
2020-06-07 20:25:28 -0400

    Update Tank.js
    added:
    - code to go back to the potion shop when low on potions
    - code to taunt enemies

--- Tank.js
+++ Tank.js
@@ -7,11 +7,33 @@ var attack_mode=false
 ​
 setInterval(function(){
 ​
+    //TODO: optimize use_hp_mp... going to go broke chugging potions every .25 seconds
     use_hp_or_mp();
     loot();
 ​
     if(!attack_mode || character.rip || is_moving(character)) return;
 ​
+
+    //code to buy potions
+    //goes to shop when magic or health pots <5 then buys 200 of each  as needed
+	if(item_quantity("mpot0")<5 || item_quantity("hpot0")<5) // item_quantity is 					defined below
+	{
+		smart_move({to:"potions",return:true},function()
+				   {	
+						if(item_quantity("mpot0")<5)
+							buy("mpot0",200);
+						if(item_quantity("hpot0")<5)
+							buy("hpot0",200);
+					});
+		// {to:"potions"} is ~equal to {"map":"main","x":56,"y":-122}
+		// {return:true} brings you back to your original position
+		// while the smart_move is happening, is_moving is false
+		// therefore the attack routine doesn't execute
+		// when the smart_move destination is reached
+		// buy("mpot0",10); executes and buys 10 potions
+		return;
+	}
+
     var target=get_targeted_monster();
     if(!target)
     {
@@ -35,6 +57,11 @@ setInterval(function(){
     else if(can_attack(target))
     {
         set_message("Attacking");
+        if(!target.taunted)
+		{
+			use_skill("taunt",target);
+			//say("TAUNTING");
+		}
         attack(target);
     }
 ​
